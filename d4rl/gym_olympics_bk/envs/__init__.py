05660d57fba8320db921c5959f472127929975a5
CZtheHusky <huskyc@qq.com>
2022-09-05 09:07:38 +0800

    remove outdated olympics env

--- d4rl/gym_olympics_bk/envs/__init__.py
+++
@@ -1,7 +0,0 @@
-from d4rl.gym_olympics.envs.running import running
-from d4rl.gym_olympics.envs.table_hockey import table_hockey
-from d4rl.gym_olympics.envs.football import football
-from d4rl.gym_olympics.envs.wrestling import wrestling
-from d4rl.gym_olympics.envs.rd_running import rd_running
-from d4rl.gym_olympics.envs.billiard_joint import billiard_joint
-from d4rl.gym_olympics.envs.curling_competition import curling_competition
