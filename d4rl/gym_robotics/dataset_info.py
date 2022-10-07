47e823723dcdb42611aabce96a7ee94a8493a9b0
Ming Zhou <kornbergfresnel@outlook.com>
2022-10-08 00:23:38 +0800

    update dataset info

--- d4rl/gym_robotics/dataset_info.py
+++ d4rl/gym_robotics/dataset_info.py
@@ -1,11 +1,14 @@
 DATASET_URLS = {
     "a1-default": "/raid/robotics/dataset/a1-default.pkl",
+    "a1-average": "/raid/robotics/dataset/a1-average.hdf5"
 }
 
 REF_MIN_SCORE = {
     "a1-default": 0.13609728,
+    "a1-average": 0.0
 }
 
 REF_MAX_SCORE = {
     "a1-default": 24.51226,
+    "a1-average": 593.7071
 }
